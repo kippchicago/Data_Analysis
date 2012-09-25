b6303e2daf6a8c154bc6e886c4ddd1922715100f
Chris Haid <chaid@kippchicago.org>
2012-09-25 14:27:14 -0500

    Got map_cdf_prep.sh finally work, added new match-replace pairs. Also symlinked to /usr/local/bin to allow global call to it.

--- MAP/Code/SQL/map_cdf_prep.sh
+++ MAP/Code/SQL/map_cdf_prep.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-#shell script that cleans NWEA provided comprehensive data files (CDFs) for loading into a MySQL Database
+#shell script that cleans or polishes NWEA provided comprehensive data files (CDFs) for loading into a MySQL Database
 
-#USAGE: dataprep foo.csv
-#input: foo.csv, which is any CFD csv
-#output: foo_loaddata.csv, a csv ready to load into a MySQL data base. 
+#USAGE| dataprep foo.csv
+#input| foo.csv, which is any CFD csv
+#output| foo_loaddata.csv, a csv ready to load into a MySQL data base. 
 
 #partion and save file name into twoparts (for foo.csv, first = foo and rest = .csv)
-first=`echo $1 | sed 's:\(.*\)\(\..*\):\1:'`
-rest=`echo $1 | sed 's:\(.*\)\(\..*\):\2:'`
+first=`echo $1 | sed 's|\(.*\)\(\..*\)|\1|'`
+rest=`echo $1 | sed 's|\(.*\)\(\..*\)|\2|'`
 
-#long sed using regexp to remove spaces and slashes from column headers and replaces cells that are empty or contain ", BR, * with \N
-sed -E -e 's:,NumSense,:_NumSense_:g' -e 's:,Stat/Prob,:_Stat_Prob_:g' -e 's:,Phonic,:_Phonic_:g'  -e 's:,"",:,\\N,:g' -e 's:"::g' -e 's:BR:\\N:g' -e 's:*:\N:g'  -e '1d'  <$1 >$first'_loaddata'$rest
+#long sed using regexp to remove spaces and slashes, commas, and slashes for cells. Also replaces cells that are empty or contain ", BR, * with \N
+sed -E -e 's|_NumSense_,NumSense,||g' -e 's|,Stat/Prob,|_Stat_Prob_|g' -e 's|,Phonic,|_Phonic_|g'  -e 's|,"",|,\\N,|g' -e 's|"||g' -e 's|BR|\\N|g' -e 's| Grammar, Spelling|_Grammar_Spelling|g' -e 's| Punctuation, Cap., Struct.|_Punctuation_Cap_Struct|g'  <$1 >$first'_loaddata'$rest
 
 #messsage upon completion
-echo 'Data from '$1' cleaned and saved to '$first'_loaddata'$rest'. BOOYAH. Time Saved! CHANGED?'
+echo 'Data from '$1' cleaned and saved to '$first'_loaddata'$rest'. BOOYAH. Time Saved!'
 
 
