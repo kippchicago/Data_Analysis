65d0a018d60074d3386dac1c2d68746cf30d811d
Chris Haid <chaid@kippchicago.org>
2012-09-26 07:35:04 -0500

    Finally got map_cdf_prep.sh working properly.  The new CDF files use DOS's embedded ^M character to denote new lines, which *Nix systems do not recognize.

--- MAP/Code/SQL/map_cdf_prep.sh
+++ MAP/Code/SQL/map_cdf_prep.sh
@@ -10,10 +10,16 @@
 first=`echo $1 | sed 's|\(.*\)\(\..*\)|\1|'`
 rest=`echo $1 | sed 's|\(.*\)\(\..*\)|\2|'`
 
-#long sed using regexp to remove spaces and slashes, commas, and slashes for cells. Also replaces cells that are empty or contain ", BR, * with \N
-sed -E -e 's|_NumSense_,NumSense,||g' -e 's|,Stat/Prob,|_Stat_Prob_|g' -e 's|,Phonic,|_Phonic_|g'  -e 's|,"",|,\\N,|g' -e 's|"||g' -e 's|BR|\\N|g' -e 's| Grammar, Spelling|_Grammar_Spelling|g' -e 's| Punctuation, Cap., Struct.|_Punctuation_Cap_Struct|g'  <$1 >$first'_loaddata'$rest
+newfile=$first'_loaddata'$rest
 
+#remove stupid DOS formatting (paritcularly the ^M new line command )
+tr '\r' '\n' <$1 >$newfile
+#long sed using regexp to remove spaces and slashes, commas, and slashes for cells. Also replaces cells that are empty or contain ", BR, * with \N, then deletes the first line of the file.
+sed -E -i "" -e 's|_NumSense_,NumSense,||g'  -e 's|,Stat/Prob,|_Stat_Prob_|g' -e 's|,Phonic,|_Phonic_|g'  -e 's|,"",|,\\N,|g' -e 's|"||g' -e 's|BR|\\N|g' -e 's|, Statistics, and Probability|_Statistics_and_Probability|g' -e 's| Grammar, Spelling|_Grammar_Spelling|g' -e 's| Punctuation, Cap., Struct.|_Punctuation_Cap_Struct|g' $newfile
+
+#remove first line of table headers
+sed -i "" '1d' $newfile  
 #messsage upon completion
-echo 'Data from '$1' cleaned and saved to '$first'_loaddata'$rest'. BOOYAH. Time Saved!'
+echo 'Data from '$1' cleaned and saved to '$newfile'. BOOYAH. Time Saved!'
 
 
