7ffcf51f95847d180ff25e773d2f30530796672e
Chris Haid <chaid@kippchicago.org>
2012-09-26 11:48:35 -0500

    Added substituion-replace from 'Science - General Science' to 'General Science' to map_cdf_prep.sh

--- MAP/Code/SQL/map_cdf_prep.sh
+++ MAP/Code/SQL/map_cdf_prep.sh
@@ -12,12 +12,13 @@ rest=`echo $1 | sed 's|\(.*\)\(\..*\)|\2|'`
 
 newfile=$first'_loaddata'$rest
 
-#remove stupid DOS formatting (paritcularly the ^M new line command )
+#remove stupid DOS formatting (paritcularly the ^M new line command ) or post Fall 2012 Web based map.  This line can be commented out for 
+#any CDF files prior to Fall 2012
 tr '\r' '\n' <$1 >$newfile
 #long sed using regexp to remove spaces and slashes, commas, and slashes for cells. Also replaces cells that are empty or contain ", BR, * with \N, then deletes the first line of the file.
-sed -E -i "" -e 's|_NumSense_,NumSense,||g'  -e 's|,Stat/Prob,|_Stat_Prob_|g' -e 's|,Phonic,|_Phonic_|g'  -e 's|,"",|,\\N,|g' -e 's|"||g' -e 's|BR|\\N|g' -e 's|, Statistics, and Probability|_Statistics_and_Probability|g' -e 's| Grammar, Spelling|_Grammar_Spelling|g' -e 's| Punctuation, Cap., Struct.|_Punctuation_Cap_Struct|g' $newfile
+sed -E -i "" -e 's|_NumSense_,NumSense,||g'  -e 's|,Stat/Prob,|_Stat_Prob_|g' -e 's|,Phonic,|_Phonic_|g'  -e 's|,"",|,\\N,|g' -e 's|"||g' -e 's|BR|\\N|g' -e 's|, Statistics, and Probability|_Statistics_and_Probability|g' -e 's| Grammar, Spelling|_Grammar_Spelling|g' -e 's| Punctuation, Cap., Struct.|_Punctuation_Cap_Struct|g' -e 's|Science - General Science|General Science|g' $newfile
 
-#remove first line of table headers
+#remove first line of table headers f 
 sed -i "" '1d' $newfile  
 #messsage upon completion
 echo 'Data from '$1' cleaned and saved to '$newfile'. BOOYAH. Time Saved!'
