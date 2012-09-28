bf39b504298be37da7f7cf2e39c46766f26dbe40
Chris Haid <chaid@kippchicago.org>
2012-09-28 16:18:37 -0500

    Lots of improvements, including adding the comparative density plots, making functions of them, and debugging the hell out of them.

--- MAP/Code/R/MAP_helper_functions.R
+++ MAP/Code/R/MAP_helper_functions.R
@@ -32,7 +32,7 @@ get_group_stats<-function(df, grp="Quartile",RIT="Fall12_RIT"){
 
 
 #Function to report MAP RIT scores and goals (expected adn expected at 7th percentile) with summary stats
-plot_MAP_Results_and_Goals <- function (df, plottitle=" ",labxpos=115, minx=105) {
+plot_MAP_Results_and_Goals <- function (df, plottitle=" ",labxpos=115, minx=105,alp=1) {
   kippcols<-c("#E27425", "#FEBC11", "#255694", "A7CFEE")
   
   #Plot points for Fall RIT Score, Expected Growth, College Ready Growth, ordered by Fall RIT, Names on Y axis
@@ -41,8 +41,8 @@ plot_MAP_Results_and_Goals <- function (df, plottitle=" ",labxpos=115, minx=105)
     geom_text(aes(x=Fall12_RIT-1, color=as.factor(Quartile), label=StudentFirstLastName), size=2, hjust=1) +
     geom_point(aes(color=as.factor(Quartile)), size=pointsize) +
     geom_text(aes(x=Fall12_RIT+1, color=as.factor(Quartile), label=Fall12_RIT), size=2, hjust=0) +
-    geom_point(aes(x=Fall12_RIT + TypicalFallToSpringGrowth, y=OrderID), color="#CFCCC1", size=pointsize) +
-    geom_text(aes(x=Fall12_RIT + TypicalFallToSpringGrowth+1, label=round(Fall12_RIT + TypicalFallToSpringGrowth)), color="#CFCCC1", size=2, hjust=0) +
+    geom_point(aes(x=Fall12_RIT + TypicalFallToSpringGrowth, y=OrderID), color="#CFCCC1", size=pointsize,, alpha=alp) +
+    geom_text(aes(x=Fall12_RIT + TypicalFallToSpringGrowth+1, label=round(Fall12_RIT + TypicalFallToSpringGrowth)), color="#CFCCC1", size=2, hjust=0, alpha=alp) +
     geom_point(aes(x=GrowthTargets, y=OrderID), color="#FEBC11", size=pointsize) + 
     geom_text(aes(x=GrowthTargets+1, label=GrowthTargets), color="#FEBC11", size=2, hjust=0) +
     facet_grid(Quartile~., scale="free_y", space = "free_y", as.table=FALSE) +
@@ -91,3 +91,77 @@ plot_MAP_Results_and_Goals <- function (df, plottitle=" ",labxpos=115, minx=105)
   
   p
 }
+
+
+##This function provides data for national refernce histogram
+
+map_2011_simulated_distr <- function (m=212.9,s=14.8) {
+  
+  set.seed(120911) # for replicability's sake
+  
+  U<-280 #upper limit on RIT scale (I've imposed this)
+  L<-120 #lower limit on RIT scale (I've imposed this)
+  n<-5000
+  
+  #m<-212.9 #5th Grade Math Mean, Fall 2011 Norms
+  #s<-14.8 #5th Grade Math SD, Fall 2011 Norms
+  
+  #need to get cumulative probablity of upper and lower bounds from normal CDF with mean m and sd s
+  p_U<-pnorm(U, mean=m, sd=s)
+  p_L<-pnorm(L, mean=m, sd=s)
+  
+  #1. draw 1000 rv distributed uniform between p_L and p_U.
+  #2. Compute the reverse CDF (normal) to get distribution of scores  
+  df.norm.ref<-data.frame(as.numeric(qnorm(runif(n,p_L,p_U),mean=m,sd=s)),rep("National Norm", n))
+  
+  names(df.norm.ref)<-c("RIT", "ID")
+  df.norm.ref$RIT<-round(df.norm.ref$RIT,0) #round to integer
+  return(df.norm.ref)
+}
+
+###Combin KIPP and National Distr data in a dataframe.  Norms data must have fields 'Mean', 'SD', 'Grade' and 'Subject'.
+
+map_combined_histo_data <- function (kippdata=map.scores.KAMS, normsdata=nwea.norms.fall, grade=5, subj="Mathematics", schoolname="KAMS") {
+  df.norm<-map_2011_simulated_distr(m=subset(normsdata, Grade==grade & Subject==subj)$Mean,s=subset(normsdata, Grade==grade & Subject==subj)$SD)
+  #get g Grade Data by schools 
+  df.norm$Subject<-subj
+  df.norm$Grade<-grade
+  
+  RIT<-subset(kippdata, Subject==subj & Grade==grade)[,"Fall12_RIT"]
+  ID<-rep(schoolname,length(RIT))
+  Subject<-rep(subj,length(RIT))
+  Grade<-rep(grade,length(RIT))
+  
+  df.kipp<-data.frame(RIT=RIT, ID=ID, Subject=Subject,Grade=Grade)
+  
+  df.combined<-rbind(df.kipp,df.norm)
+  return(df.combined)
+}
+
+
+####And now for the small mulitples, comparative histograms
+
+map_comparative_histograms <- function (df, legendpos="bottom", title=" ", schoolname="KAMS",...) {
+  pctl75<-quantile(x=df$RIT,probs=.75)
+  bw<-2 #binning width
+  #df.plot.data$CollegeReady<-df.plot.data$RIT>pctl75
+  
+  df.vline <- data.frame(ID=unique(df$ID), vl=c(mean(subset(df, ID==schoolname)$RIT),mean(subset(df, ID=="National Norm")$RIT)))
+  #KIPP friendly colors
+  kippcols <- c("#CFCCC1","#60A2D7")
+  
+  #these next two lines allow me to pass the variable pctl75 to geom_hist, rather than hardcoding anything!
+  fill.text<-paste("..x..>",pctl75,sep="")
+  y.text<-"..density.."
+  
+  p <- ggplot(df, aes(x=RIT)) 
+  p <- p + geom_histogram(aes_string(y = y.text, fill=fill.text),  binwidth=bw)  + 
+    facet_grid(ID~.) +
+    geom_vline(x=pctl75+(.5*bw), color="#E27425") +
+    geom_vline(data=df.vline, aes(xintercept=vl), color="#439539") +
+    scale_fill_manual("College Readiness",values = kippcols, labels=c("< 75th Percentile", "> 75th Percentile")) +
+    opts(legend.position=legendpos,
+         title=title) 
+  p
+}
+
