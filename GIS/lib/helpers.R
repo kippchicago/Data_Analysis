13069275678737189b34ef7346b617a838fdacc6
Christopher Haid <chaid@kippchicago.org>
2014-03-01 12:45:39 -0600

    Added insets to map and an insetting function (asks for zoom level and extent of zoom area, and location of zooming box. Next step is to make inset_zoom work like a normal ggplot2 function (just add with a +).

--- GIS/lib/helpers.R
+++ GIS/lib/helpers.R
@@ -1,4 +1,166 @@
-helper.function <- function()
-{
-  return(1)
+#a function that returns student number based on the best match
+best_match <- function(input_name, .data, match_column, output_column, confidence){
+  if(!require(RecordLinkage)) stop("Please install package RecordLinakge.")
+  #fuzzy match on strings
+  
+  
+  m_col<-as.character(substitute(match_column))
+  o_col<-as.character(substitute(output_column))
+  
+  
+  .data[,m_col]
+  
+  distance <- levenshteinSim(input_name, 
+                             as.character(.data[,m_col]))
+  
+  best <- .data[, m_col][distance == max(distance)]
+  
+  #name of matc
+  #match_name <- max(.data[which(.data[,m_col]==best), m_col])
+  
+  #display to console if not an exact match
+  if (max(distance) < 1.0) {
+    #build string
+    disp_str <- paste(
+      input_name , 'matched to',
+      best, 'with confidence', round(max(distance), 2), '\n'
+    )
+    #print 
+    message(disp_str)
+  }
+  
+  #get student number from attrition data frame that matches
+  out <- max(.data[which(.data[,m_col]==best),o_col])
+  
+  #return NA if below confidence
+  if (max(distance) >= confidence) {
+    return(out)
+  } else {
+    return(NA)
+  }
 }
+
+gg_color_hue <- function(n) {
+  hues = seq(15, 375, length=n+1)
+  hcl(h=hues, l=65, c=100)[1:n]
+}
+
+inset_zoom<-function(
+  p,  		# ggplot object					######
+  zoom = 2,					# The zoom level of the expand box			######
+  pad = 1,						# Padding between exp box and plot box		######
+  ex.col = "white",			# Fill colour of expand boxes				######
+  ex.alpha = 0,				# Turn to zero to turn off any colour		######
+  ex.lin = "black",			# Line colour of expand boxes				######
+  x.expand = c(3, 4),			# Desired expand range (X)					######
+  y.expand = c(13, 16),		# Desired expand range (y)					######
+  j.expand = "br"	){
+  
+  p.grob<-ggplot_build(p)
+  xlims <- p.grob$panel$ranges[[1]]$x.range
+  ylims <- p.grob$panel$ranges[[1]]$y.range
+  
+  ##########################################################
+  # DO NOT EDIT - Computation of expanded box coords #######
+  ##########################################################
+  if (j.expand == "tr") {  							######
+                                  xl = xlims[2]-((x.expand[2]-x.expand[1])*zoom)		######
+                                  xh = xlims[2]										######
+                                  yl = ylims[2]-((y.expand[2]-y.expand[1])*zoom)		######
+                                  yh = ylims[2]										######
+                                  padl = -1											######
+                                  padr = pad											######
+                                  padt = pad											######
+                                  padb = -1											######
+  }													######
+  ######
+  if (j.expand == "bl") {								######
+                                 xl = xlims[1]										######
+                                 xh = xlims[1]+((x.expand[2]-x.expand[1])*zoom)		######
+                                 yl = ylims[1]										######
+                                 yh = ylims[1]+((y.expand[2]-y.expand[1])*zoom)		######
+                                 padl = pad-1										######
+                                 padr = -1											######
+                                 padt = -1											######
+                                 padb = pad-1										######
+  }													######
+  ######
+  if (j.expand == "tl") {								######
+                                 xl = xlims[1]										######
+                                 xh = xlims[1]+((x.expand[2]-x.expand[1])*zoom)		######
+                                 yl = ylims[2]-((y.expand[2]-y.expand[1])*zoom)		######
+                                 yh = ylims[2]										######
+                                 padl = pad-1										######
+                                 padr = -1											######
+                                 padt = pad											######
+                                 padb = -1											######
+  }													######
+  ######
+  if (j.expand == "br") {								######
+                                 xl = xlims[2]-((x.expand[2]-x.expand[1])*zoom)		######
+                                 xh = xlims[2]										######
+                                 yl = ylims[1]										######
+                                 yh = ylims[1]+((y.expand[2]-y.expand[1])*zoom)		######
+                                 padl = -1											######
+                                 padr = pad											######
+                                 padt = -1											######
+                                 padb = pad-1										######
+  }													######
+  ##########################################################
+  ##########################################################
+  ##########################################################
+  
+  #here we do that acutal zooming
+  p.zoom <- p +
+    coord_cartesian (xlim = x.expand, ylim = y.expand) +
+    annotate("text",   					#IMPORTANT
+             x=x.expand[2], 						#IMPORTANT
+             y = y.expand[1], 					#IMPORTANT
+             label = paste("x", zoom, sep=""), 	#IMPORTANT
+             hjust = 1.25, 						#IMPORTANT
+             vjust = -0.4						#IMPORTANT
+    ) +				
+    theme (
+      # Plot Attributes
+      plot.title = element_text (size= 10),										#IMPORTANT
+      plot.margin = unit (c(padt, padr, padb, padl), "lines"), #T, R, B, L		#IMPORTANT
+      plot.background = element_blank (), 										#IMPORTANT
+      
+      # Panel Attributes	
+      panel.grid.major = element_blank (), 										# Removes major grid
+      panel.grid.minor = element_blank (),  										# Removes minor grid
+      panel.background = element_rect (fill = ex.col),							#IMPORTANT
+      panel.border = element_rect (colour = ex.lin, fill = F, size = 0.5),		#IMPORTANT	
+      
+      #Legend Attributes		
+      legend.position = "none",											#IMPORTANT
+      
+      
+      # Axis Attributes
+      axis.title.x = element_blank (),	#IMPORTANT
+      axis.title.y = element_blank (), 	#IMPORTANT
+      axis.text.x = element_blank (), 	#IMPORTANT
+      axis.text.y = element_blank (), 	#IMPORTANT
+      axis.ticks = element_blank ()		#IMPORTANT
+    )
+  
+  p.zoom <- ggplotGrob (p.zoom)
+  
+  #add these to orginial plot
+  # area zoomed added to original plot
+  p + annotate("rect",   		#IMPORTANT
+               xmin = x.expand[1], 	#IMPORTANT
+               xmax = x.expand[2], 	#IMPORTANT
+               ymin = y.expand[1], 	#IMPORTANT
+               ymax = y.expand[2], 	#IMPORTANT
+               fill = ex.col, 			#IMPORTANT
+               colour = ex.lin,		#IMPORTANT
+               alpha = ex.alpha		#IMPORTANT
+  ) + 
+    annotation_custom(p.zoom, 
+                      xmin=xl, 
+                      xmax=xh, 
+                      ymin=yl, 
+                      ymax=yh)  
+}
+
--- GIS/src/busing_map.R
+++ GIS/src/busing_map.R
@@ -1,3 +1,4 @@
+# Load packages ####
 require(ggplot2)
 require(maptools)
 require(rgeos)
@@ -6,8 +7,12 @@ require(ggmap)
 require(RJDBC)
 require(data.table)
 
+gpclibPermit()
 
-# get student data from PowerSChool
+setwd("~/Dropbox/Consulting/KIPP Ascend/Data Analysis/GIS")
+
+
+# get student data from PowerSchool ####
 drvr <- JDBC("oracle.jdbc.driver.OracleDriver", "/Users/chaid/Dropbox/JDBC Drivers/ojdbc6.jar","") # define driver
 
 pspw <- as.list(read.dcf("config/ps.dcf", all=TRUE)) #read DCF with configuration settings
@@ -36,21 +41,68 @@ students[SCHOOLID==400163,School:="KBCP"]
 students[,Address:=paste(gsub("(.+),(.+)", "\\1", students$STREET), ZIP)]
 
 
-#get shape files
+# get shape files ####
 chi_boundaries.rg<-readOGR("shapefiles/City_Boundary", "City_Boundary")
 
+#get zip codes
 zip_codes<-readOGR("shapefiles/ZipCodes", "ZipCodes")
-
-
+# prep zipcodes for ggplot
 zip_codes@data$id <- rownames(zip_codes@data)
-
 zips <- fortify(zip_codes, region="id")
-
 zips.df<-merge(zips, zip_codes, by="id")
-
 zips.dt<-data.table(zips.df[order(zips.df$id, zips.df$order),])
 
-#geocode addresses
+# get major streets
+major_streets <- readOGR("shapefiles/Major_Streets/", "Major_Streets")
+
+major_streets@data$id <- rownames(major_streets@data)
+streets <- fortify(major_streets, region = "id")
+streets.df <- merge(streets, major_streets, by="id")
+
+streets.dt<-data.table(streets.df[order(streets.df$id, streets.df$order),])
+streets.data.dt<-data.table(major_streets@data)
+
+setkey(streets.dt, "id")
+setkey(streets.data.dt, "id")
+
+streets.dt<-streets.dt[streets.data.dt]
+
+# CPS Schools ####
+cps_schools <- readOGR("shapefiles/School_Grounds/", "School_Grounds")
+
+cps_schools@data$match_col<-with(cps_schools@data, paste(SCHOOL_NAM, PRIMARY_AD))
+
+CPS.Schools[,match_col:=paste(INCS_School_Name, Street_Address)]
+CPS.Schools[,School_Type:=as.character(INCS_School_Type)]
+
+school_type<-unlist(lapply(as.list(cps_schools$match_col), 
+                           best_match, 
+                           .data=as.data.frame(CPS.Schools[INCS_School_Type=="Charter"]), 
+                           match_column=match_col, 
+                           output_column=School_Type, 
+                           confidence=.6))
+
+
+cps_schools@data$id <- rownames(cps_schools@data)
+cps.df <- fortify(cps_schools, region = "id")
+cps_schools.df <- merge(cps.df, cps_schools, by="id")
+
+#cps_schools.data.dt<-data.table(cps_schools@data)
+cps.dt<-data.table(cps_schools.df)
+cps.dt[,School_Type2:=ifelse(School_Type=="Charter", "Charter", "Non-Charter")]
+cps.dt[,School_Type2_col:=ifelse(School_Type2=="Charter", "black", "#C49A6C")]
+
+charter_lonlat<-as.matrix(CPS.Schools[INCS_School_Type=="Charter",list(Long, Lat)])
+charter_reprojected<-project(charter_lonlat, proj=p4)
+CPS.Schools[INCS_School_Type=="Charter", long:=charter_reprojected[,1]]
+CPS.Schools[INCS_School_Type=="Charter", lat:=charter_reprojected[,2]]
+
+# 
+
+
+
+
+# geocode addresses ####
 addr_geocoded<-geocode(students$Address)
 
 # reproject_addresses
@@ -79,6 +131,7 @@ zips.dt[ZIP %in% c(60638, 60632, 60609, 60653, 60615,
                    60655, 60643, 60628, 60827), 
         School:="KBCP"]
 
+
 school_location<-data.table(School=c("KAMS", "KCCP", "KBCP"), Address=c("1616 S Avers Ave 60623", 
                                                                           "4818 W Ohio St 60644",
                                                                           "5515 S Lowe Ave 60621"))
@@ -90,17 +143,54 @@ school_location[,long:=schools.reprojected[,1]]
 school_location[,lat:=schools.reprojected[,2]]
 
 
+# bus stops
+bus_stops.geocoded <- geocode(bus.stops[,paste(Stop_address, City_State)])
+bus_stops.reprojected<-project(as.matrix(bus_stops.geocoded), proj=p4)
+bus.stops[,long:=bus_stops.reprojected[,1]]
+bus.stops[,lat:=bus_stops.reprojected[,2]]
+
+
+
+cols<-gg_color_hue(3)
+
+students.clipped[School=="KAMS",School_col:=cols[1]]
+students.clipped[School=="KCCP",School_col:=cols[2]]
+students.clipped[School=="KBCP",School_col:=cols[3]]
+
+zips.dt[School=="KAMS",School_col:=cols[1]]
+zips.dt[School=="KCCP",School_col:=cols[2]]
+zips.dt[School=="KBCP",School_col:=cols[3]]
+
+school_location[School=="KAMS",School_col:=cols[1]]
+school_location[School=="KCCP",School_col:=cols[2]]
+school_location[School=="KBCP",School_col:=cols[3]]
+
 p <- ggplot(data=students.clipped[School!="KAP"], aes(x=long, y=lat)) + 
-  geom_polygon(data=zips.dt, aes(x=long, y=lat, group=group), color="gray", fill=NA) + 
-  geom_polygon(data=zips.dt[!is.na(School)], aes(x=long, y=lat, group=group, color=School, fill=School), alpha=.1) + 
-  geom_point(aes(x=long, y=lat, color=School), alpha=.6, size=3) + 
-  geom_point(data=school_location, aes(x=long, y=lat, fill=School), color="black", shape=23, size=6, alpha=.5) +
+  geom_segment(data=major_streets_lines, aes(x=slon, y=slat, xend=elon, yend=elat), color="lightgray", lineend = "round") +
+  geom_polygon(data=zips.dt, aes(x=long, y=lat, group=group), color="darkgray", fill=NA) + 
+  geom_polygon(data=zips.dt[!is.na(School)], aes(x=long, y=lat, group=group, color=School_col, fill=School_col), alpha=.1) + 
+  geom_point(aes(x=long, y=lat, color=School_col), alpha=.6, size=1.75) + 
+  geom_point(data=school_location, aes(x=long, y=lat, fill=School_col), color="black", shape=23, size=6, alpha=.5) +
   geom_text(data=school_location, aes(x=long, y=lat, label=School), hjust=0, vjust=0, alpha=.5) +
+  geom_polygon(data=cps.dt, aes(x=long, y=lat, group=group), fill="black") +
+  scale_fill_identity() + 
+  scale_color_identity() +
   #facet_wrap(~GRADE_LEVEL) + 
   theme_minimal()
 
-pdf("graphs/busing_map.pdf", height=11, width=8.5)
-p + ggtitle("KIPP Chi: Students by School\nwith Busing ZIP Codes Highlighted")
+
+
+p.buses <- p +   geom_point(data=CPS.Schools[INCS_School_Type=="Charter" & lat>1200000], aes(long, lat), 
+                 color="#FEBC11", shape=8, size=2) +  
+  geom_point(data=bus.stops, aes(x=long, y=lat, alpha=Route), shape=13, color="purple", fill="purple", size=3) +
+  ggtitle("KIPP Chi: Students by School\nwith Busing ZIP Codes Highlighted")
+
+pdf("graphs/busing_map_with_schools.pdf", height=11, width=8.5)
+inset_zoom(p.buses, 
+           zoom=1.5, 
+           x.expand=c(1140000, 1165000), 
+           y.expand=c(1885000, 1915000), 
+           j.expand="bl")
 
 p + facet_wrap(~GRADE_LEVEL) + ggtitle("KIPP Chi: Students by Grade & School\nwith Busing ZIP Codes Highlighted")
 dev.off()
